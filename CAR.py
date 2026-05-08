edfda5cf24ffc5d3304a4549323b733bea0546fa
jeffweng-blip <jeffweng@mitac.com.tw>
2026-05-08 16:39:12 +0800

    Update CAR.py

--- CAR.py
+++ CAR.py
@@ -141,10 +141,10 @@ def generate_overlay_pdf():
         em = parts[-1].strip() if len(parts) > 2 else "" # 結束分
         
         # 填入對應「時」、「分」前方的空格
-        c.drawString(190, 625, sh)  # 開始小時
-        c.drawString(245, 625, sm)  # 開始分鐘
-        c.drawString(310, 625, eh)  # 結束小時
-        c.drawString(375, 625, em)  # 結束分鐘
+        c.drawString(190, 623, sh)  # 開始小時
+        c.drawString(245, 623, sm)  # 開始分鐘
+        c.drawString(310, 623, eh)  # 結束小時
+        c.drawString(365, 623, em)  # 結束分鐘
     except:
         # 萬一格式異常，則退回原顯示方式
         c.drawString(275, 620, selected_time.replace(':', ' ').replace('~', ' '))
